2f61addc5d4274c5e872b99cafe7e072268ae290
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-09-03 09:38:47 +0530

    1 lecture

---
+++ basic c++/area clculate.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    int area;
+    cout<<"ennter a number";
+    cin>>a;
+    cout<<"ennter a number";
+    cin>>b;
+    area=a*b;
+
+    cout<<"clculte area"<<area;
+    return 0;
+
+}
