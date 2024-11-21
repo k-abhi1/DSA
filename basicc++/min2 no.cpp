50b2940696518779e6968758c820b0f1a76c99a8
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-11-21 23:38:35 +0530

    s

---
+++ basicc++/min2 no.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cout<<"enter a number";
+    cin>>a;
+
+     cout<<"enter a number";
+     cin>>b;
+
+    if(a<=b)
+    {
+        cout<<"min is"<<a;
+    }
+    else
+    {
+        cout<<"min is"<<b;
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
