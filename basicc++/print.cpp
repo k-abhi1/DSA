39913aae94d88e4ace224b6c89e15578d782ce4c
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-11-22 21:40:17 +0530

    second day

---
+++ basicc++/print.cpp
@@ -0,0 +1,14 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"abhishek\n kumar";
+// }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Abhishek"<<endl<< "Kumar"<<endl;
+    return 0;
+}
---
+++ basicc++/sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespce std;
+ int main(){
+    int a,b,sum;
+    cout<<"enter the first number";
+    cin>>a;
+    cout<<"enter the secon number";
+    cin>>b;
+
+    sum=a+b;
+    cout<<sum<<endl;
+
+    return 0;
+ }
